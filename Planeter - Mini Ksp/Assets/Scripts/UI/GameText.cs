6662f27091191f04e5f628668d397c6a76918a23
Felix Voigtländer <voigtlaender.felix01@googlemail.com>
2020-11-07 17:25:33 +0100

    Build test preparation

---
+++ Planeter - Mini Ksp/Assets/Scripts/UI/GameText.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameText : MonoBehaviour
+{
+    [TextArea]
+    public string inGameText = "";
+    [TextArea]
+    public string outGameText = "";
+    public TextMeshProUGUI textField;
+
+    public void Start()
+    {
+        textField = GetComponent<TextMeshProUGUI>();
+
+        GameManager.OnGameStart += OnGameStart;
+        GameManager.OnGameEnd += OnGameEnd;
+    }
+
+    public void OnGameStart()
+    {
+        SetText(inGameText);
+    }
+    public void OnGameEnd()
+    {
+        SetText(outGameText);
+    }
+    public void SetText(string text)
+    {
+        textField.SetText(text);
+    }
+
+
+}
---
+++ Planeter - Mini Ksp/Assets/Scripts/UI/OpenHyperlinks.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class OpenHyperlinks : MonoBehaviour, IPointerClickHandler
+{
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        TMP_Text pTextMeshPro = GetComponent<TMP_Text>();
+        int linkIndex = TMP_TextUtilities.FindIntersectingLink(pTextMeshPro, eventData.position, null);  // If you are not in a Canvas using Screen Overlay, put your camera instead of null
+        if (linkIndex != -1)
+        { // was a link clicked?
+            TMP_LinkInfo linkInfo = pTextMeshPro.textInfo.linkInfo[linkIndex];
+            Application.OpenURL(linkInfo.GetLinkID());
+        }
+    }
+}
