b3adda91c2abfdd7664e6d357e55292b8945b7c6
Felix Voigtländer <voigtlaender.felix01@googlemail.com>
2020-11-22 20:17:23 +0100

    Configured Planet Prefabs

--- Planeter - Mini Ksp/Assets/Scripts/Game/CameraSelectable.cs
+++ Planeter - Mini Ksp/Assets/Scripts/Game/CameraSelectable.cs
@@ -8,6 +8,11 @@ public class CameraSelectable : MonoBehaviour
 
     public bool selectOnStart = false;
 
+    public bool useWorldSpace = true;
+
+
+    
+
     private void Start()
     {
         if (selectOnStart)
@@ -16,11 +21,12 @@ public class CameraSelectable : MonoBehaviour
 
     private void Update()
     {
+        // Phone Input
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
         {
             ManageInput(Input.GetTouch(0).position);
         }
-
+        // Mouse Input
         if (Input.GetMouseButtonDown(0))
         {
             ManageInput(Input.mousePosition);
@@ -28,6 +34,15 @@ public class CameraSelectable : MonoBehaviour
     }
 
     public void ManageInput(Vector2 touchPixelPos)
+    {
+        if (useWorldSpace)
+            ManageWorldInput(Camera.main.ScreenToWorldPoint(touchPixelPos));
+        else
+            ManagePixelInput(touchPixelPos);
+    }
+
+    
+    public void ManagePixelInput(Vector2 touchPixelPos)
     {
         Vector2 myPixelPos = Camera.main.WorldToScreenPoint(transform.position);
 
@@ -38,4 +53,20 @@ public class CameraSelectable : MonoBehaviour
             CameraController.SetTarget(transform);
         }
     }
+    /// <summary>
+    /// Uses lossy scale as radius!
+    /// </summary>
+    /// <param name="worldPos">Position of click in world space</param>
+    public void ManageWorldInput(Vector2 worldPos)
+    {
+        float pixelDistance = ((Vector2)transform.position - worldPos).magnitude;
+        float radius = transform.lossyScale.x / 2;
+
+        if (pixelDistance < radius)
+        {
+            CameraController.SetTarget(transform);
+        }
+    }
+
+
 }
